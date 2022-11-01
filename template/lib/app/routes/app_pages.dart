f170c4232ff3eeffd8d838278e53fb3b865c28b5
pacoblomkvist <62117304+pacoblomkvist@users.noreply.github.com>
2022-11-01 14:22:03 +0100

    bye bye home

--- template/lib/app/routes/app_pages.dart
+++ template/lib/app/routes/app_pages.dart
@@ -2,9 +2,6 @@ import 'package:get/get.dart';
 import 'package:template/app/modules/example/DI.dart';
 import 'package:template/app/modules/example/views/example_view.dart';
 
-import '../modules/home/bindings/home_binding.dart';
-import '../modules/home/views/home_view.dart';
-
 part 'app_routes.dart';
 
 class AppPages {
@@ -13,11 +10,6 @@ class AppPages {
   static const INITIAL = Routes.EXAMPLE;
 
   static final routes = [
-    GetPage(
-      name: Routes.HOME,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
-    ),
     GetPage(
       name: Routes.EXAMPLE,
       page: () => ExampleView(),
